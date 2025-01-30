875170def5bc4e74c38754655232b29772684773
Sandeep <sandeepnegi686@gmail.com>
2025-01-30 12:19:05 +0530

    first commit

---
+++ 1. practice/a.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    cout << "Hello World...!" << endl;
+
+    return 0;
+}
