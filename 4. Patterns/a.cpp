f46a97710d93ff8654a7a67ee48c8c62897147bb
Sandeep <sandeepnegi686@gmail.com>
2025-02-05 11:44:33 +0530

    updated content

---
+++ 4. Patterns/a.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int n = 4;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)
+//         {
+//             cout << "*" << " ";
+//         }
+//         cout << "\n";
+//     }
+
+//     return 0;
+// }
+// int main()
+// {
+//     int n = 5;
+
+//     char ch = 'A';
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << ch << " ";
+//         }
+//         cout << "\n";
+//         ch++;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = i; j > 0; j--)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int n = 4;
+//     int num = 1;
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         char ch = 65 + i - 1;
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << ch << " ";
+//             ch--;
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int n = 4;
+//     char ch = 'A';
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n = 5;
+//     char ch = 'A';
+//     for (int i = 1; i < n + 1; i++)
+//     {
+//         for (int j = 1; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = n - i + 1; j > 0; j--)
+//         {
+//             cout << ch;
+//         }
+//         cout << "\n";
+//         ch++;
+//     }
+//     return 0;
+// }
+
+int main()
+{
+    int n = 4;
+
+    return 0;
+}
