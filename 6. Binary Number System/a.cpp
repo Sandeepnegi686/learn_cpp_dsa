0249e2cd48a491548bfb86a5c7fe569c5c32063b
Sandeep <sandeepnegi686@gmail.com>
2025-02-11 12:01:07 +0530

    updated binary numbers

--- 6. Binary Number System/a.cpp
+++ 6. Binary Number System/a.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     cout << "& " << (6 & 10) << "\n";
+//     cout << "| " << (6 | 10) << "\n";
+//     cout << "^ " << (6 ^ 10) << "\n";
+//     cout << "<< " << (10 << 2) << "\n";
+//     cout << ">> " << (10 >> 1) << "\n";
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     cout << sizeof(int) << "\n";
+//     cout << sizeof(long int) << "\n";
+//     cout << sizeof(long long int) << "\n";
+//     cout << sizeof(short int) << "\n";
+//     cout << sizeof(signed int) << "\n";
+
+//     int x = -32767;
+//     cout << x << "\n";
+//     return 0;
+// }
+
 int main()
 {
+    int n;
+    int remainder = 0;
+    bool powerOf2 = false;
 
+    cout << "enter n :";
+    cin >> n;
+
+    while (remainder = 0)
+    {
+        remainder = n % 2;
+        n = n / 2;
+    }
     return 0;
 }
