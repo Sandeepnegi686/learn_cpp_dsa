f18fb6073a8f892009ae7c111d1bd6fe46271e31
Sandeep <sandeepnegi686@gmail.com>
2025-02-23 09:51:30 +0530

    updated pointers

--- 9. Pointers/a.cpp
+++ 9. Pointers/a.cpp
@@ -41,17 +41,82 @@ using namespace std;
 // }
 
 // by reference using alias
-void changeArr(int &num)
-{
-    num = 10;
-}
+// void changeArr(int &num)
+// {
+//     num = 10;
+// }
+
+// int main()
+// {
+//     int a = 20;
+//     cout << "a = " << a << endl;
+//     changeArr(a);
+//     cout << "a = " << a;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int numbers[] = {11, 22, 33, 44};
+//     // cout << (&numbers + 1) << endl;
+//     cout << *numbers++ << endl;
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     int *ptr = &a;
+//     cout << ptr << endl;
+//     ptr++;
+//     cout << ptr << endl;
+//     ptr--;
+//     cout << ptr << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 5;
+
+//     int *ptrA = &a;
+//     int *ptrB = &b;
+
+//     ptrA++;
+
+//     cout << ptrA << endl;
+//     cout << ptrB << endl;
+//     ptrB = ptrB + 2;
+//     cout << ptrA - ptrB << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int *ptr1;
+//     int *ptr2;
+
+//     cout << ptr1 << endl;
+//     cout << ptr2 << endl;
+
+//     cout << (ptr1 > ptr2) << endl;
+
+//     return 0;
+// }
 
 int main()
 {
-    int a = 20;
-    cout << "a = " << a << endl;
-    changeArr(a);
-    cout << "a = " << a;
+    int arr[] = {10, 20, 30, 40};
+    int *ptr = arr;
+
+    cout << *(ptr + 1) << endl;
+    cout << *(ptr + 3) << endl;
+
+    ptr++;
+    cout << *ptr << endl;
 
     return 0;
 }
