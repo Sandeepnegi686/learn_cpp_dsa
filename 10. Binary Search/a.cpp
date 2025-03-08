8ce0ce56db6b5c17f47cf920136620cb6ca0a427
Sandeep <sandeepnegi686@gmail.com>
2025-03-08 11:46:05 +0530

    learn finding GCD

--- 10. Binary Search/a.cpp
+++ 10. Binary Search/a.cpp
@@ -3,50 +3,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int coutStu(vector<int> arr, int pagesPerStu)
+// int coutStu(vector<int> arr, int pagesPerStu)
+// {
+//     int std = 1;
+//     long long pagesStu = 0;
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if (pagesStu + arr[i] <= pagesPerStu)
+//         {
+//             pagesStu += arr[i];
+//         }
+//         else
+//         {
+//             std++;
+//             pagesStu = arr[i];
+//         }
+//     }
+//     return std;
+// }
+
+// int findPages(vector<int> arr, int k)
+// {
+//     int low = *max_element(arr.begin(), arr.end());
+//     int high = accumulate(arr.begin(), arr.end(), 0);
+//     while (low <= high)
+//     {
+//         int mid = low + ((high - low) / 2);
+//         int noOfStu = coutStu(arr, mid);
+//         if (noOfStu > k)
+//         {
+//             low = mid + 1;
+//         }
+//         else
+//         {
+//             high = mid - 1;
+//         }
+//     }
+//     return low;
+// }
+
+bool canWePlace(vector<int> arr, int distance, int cows)
 {
-    int std = 1;
-    long long pagesStu = 0;
-    for (int i = 0; i < arr.size(); i++)
+    int countCows = 1;
+    int lastCow = arr[0];
+    for (int i = 1; i < arr.size(); i++)
     {
-        if (pagesStu + arr[i] <= pagesPerStu)
+        if (arr[i] - lastCow <= distance)
         {
-            pagesStu += arr[i];
-        }
-        else
-        {
-            std++;
-            pagesStu = arr[i];
+            countCows++;
+            lastCow = arr[i];
         }
     }
-    return std;
+    if (countCows >= cows)
+        return 1;
+    else
+        return 0;
 }
 
-int findPages(vector<int> arr, int k)
+int findCows(vector<int> arr, int cows)
 {
-    int low = *max_element(arr.begin(), arr.end());
-    int high = accumulate(arr.begin(), arr.end(), 0);
-    while (low <= high)
+    if (cows < 2)
+        return -1;
+
+    stable_sort(arr.begin(), arr.end());
+    int max = arr[arr.size() - 1];
+    int min = arr[0];
+    for (int i = 0; i < max - min; i++)
     {
-        int mid = low + ((high - low) / 2);
-        int noOfStu = coutStu(arr, mid);
-        if (noOfStu > k)
+        if (canWePlace(arr, i, cows) == true)
         {
-            low = mid + 1;
+            continue;
         }
         else
         {
-            high = mid - 1;
+            return i - 1;
         }
     }
-    return low;
 }
 
 int main()
 {
-    vector<int> arr = {12, 34, 67, 90};
-    int k = 2;
-    cout << findPages(arr, k);
+    vector<int> arr = {1, 2, 8, 4, 9};
+    int c = 3;
+    cout << findCows(arr, c);
 
     return 0;
 }
---
+++ 11. Numbers/a.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+long long noOfDigits(int N)
+{
+    if (N == 1)
+        return 1;
+    if (N == 2)
+        return 1;
+
+    long long last = 1;
+    long long secondLast = 1;
+    long long c;
+    for (int i = 3; i <= N; i++)
+    {
+        c = last + secondLast;
+        secondLast = last;
+        last = c;
+    }
+
+    cout << "value of c : " << c << endl;
+
+    int nOfDigit = 0;
+    while (c > 0)
+    {
+        nOfDigit++;
+        c = c / 10;
+    }
+
+    return nOfDigit;
+}
+
+void printAllDivisors(int N)
+{
+    for (int i = 1; i <= sqrt(N); i++)
+    {
+        if (N % i == 0)
+        {
+            cout << i << " ";
+            if ((N % i) == 0 && (N / i) != i)
+            {
+                cout << N / i << " ";
+            }
+        }
+    }
+}
+
+bool checkPrime(int N)
+{
+    int count = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 2)
+        return true;
+    return false;
+}
+
+bool checkArmstrongNumber(int N)
+{
+    // 371 =
+    // 1634 = 1 + 216+27+64
+    int sum = 0;
+    int n = N;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        sum += lastDigit * lastDigit * lastDigit;
+        n = n / 10;
+    }
+    if (sum == N)
+        return true;
+    return false;
+}
+
+// find Greatest common Divisible
+int getGCD(int n1, int n2)
+{
+    // int ans = 1;
+    // int minNum = min(n1, n2);
+    // for (int i = 2; i <= minNum; i++)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         ans = i;
+    //     }
+    // }
+    // return ans;
+
+    int a = n1;
+    int b = n2;
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a > 0 ? a : b;
+}
+
+int main()
+{
+    // cout << noOfDigits(94) << endl;
+    // int c = 2345;
+    // int nOfDigit = 0;
+    // while (c > 0)
+    // {
+    //     nOfDigit++;
+    //     c = c / 10;
+    // }
+    // printAllDivisors(36);
+    cout << getGCD(10, 15);
+    return 0;
+}
