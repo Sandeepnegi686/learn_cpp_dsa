f09e62e38e8e345aa6bb81da79a40fc57c42f03b
Sandeep <sandeepnegi686@gmail.com>
2025-04-30 11:19:55 +0530

    updated

--- Binary Search/a.cpp
+++ Binary Search/a.cpp
@@ -3,8 +3,91 @@
 
 using namespace std;
 
+int normalBinarySearch(vector<int> nums, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    while (start <= end)
+    {
+        int mid = start + ((end - start) / 2);
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int binarySearchRecursive(vector<int> nums, int target, int start, int end)
+{
+    if (start > end)
+        return -1;
+    int mid = start + ((end - start) / 2);
+
+    if (nums[mid] == target)
+    {
+        return mid;
+    }
+    else if (nums[mid] < target)
+    {
+        return binarySearchRecursive(nums, target, mid + 1, end);
+    }
+    else if (nums[mid] > target)
+    {
+        return binarySearchRecursive(nums, target, start, mid - 1);
+    }
+}
+
+int rotatedSortedArray(vector<int> nums, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    while (start <= end)
+    {
+        int mid = start + ((end - start) / 2);
+        if (nums[mid] == target)
+            return mid;
+        if (nums[start] <= nums[mid])
+        {
+            if (nums[start] <= target && target < nums[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[mid] < target <= nums[end])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    cout << "hello";
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 5;
+    // int start = 0, end = nums.size() - 1;
+
+    cout << rotatedSortedArray(nums, target);
+
     return 0;
 }
