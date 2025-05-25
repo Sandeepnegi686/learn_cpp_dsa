cd55b7487de03db9a1bf352638e6d439ce93a662
Sandeep <sandeepnegi686@gmail.com>
2025-05-25 13:37:09 +0530

    updated content

--- Arrays/a.cpp
+++ Arrays/a.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <limits.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -116,14 +117,46 @@ int maxProfit(vector<int> nums)
     return mp;
 }
 
+void nextPermutaion(vector<int> &arr)
+{
+    int n = arr.size();
+    if (n == 1)
+        return;
+
+    int index = -1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1)
+    {
+        reverse(arr.begin(), arr.end());
+        return;
+    }
+    for (int i = n - 1; i > index; i--)
+    {
+        if (arr[i] > arr[index])
+        {
+            swap(arr[i], arr[index]);
+            break;
+        }
+    }
+    reverse(arr.begin() + index + 1, arr.end());
+}
+
 int main()
 {
-    vector<int> nums = {7, 1, 5, 3, 6, 4};
+    vector<int> nums = {1, 3, 5, 4, 2};
 
-    int ans = maxProfit(nums);
+    // int ans = nextPermutaion(nums);
 
-    // printVector(ans);
-    cout << ans;
+    nextPermutaion(nums);
+    printVector(nums);
+    // cout << ans;
 
     return 0;
 }
