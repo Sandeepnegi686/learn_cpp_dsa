6e01aa7eff3d5bdc2fc581a8f98b28af172d098f
Sandeep <sandeepnegi686@gmail.com>
2025-06-23 13:54:31 +0530

    updated matrix

--- Matrix/a.cpp
+++ Matrix/a.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <limits.h>
+#include <vector>
 
 using namespace std;
 
@@ -52,15 +53,111 @@ int maxColSum(int matrix[][3])
     return maxSum;
 }
 
+int diagonalSum(int matrix[][3], int row)
+{
+    int sum = 0;
+    if (row % 2 == 0)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            sum += matrix[i][i];
+            sum += matrix[i][row - 1 - i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < row; i++)
+        {
+            sum += matrix[i][i];
+            sum += matrix[i][row - 1 - i];
+        }
+        int centre = row / 2;
+        sum = sum - matrix[centre][centre];
+    }
+    return sum;
+}
+
+bool searchInCol(vector<vector<int>> matrix, int target, int midRow, int col)
+{
+    int st = 0, end = col - 1;
+    while (st <= end)
+    {
+        int midCol = st + (end - st) / 2;
+        if (target == matrix[midRow][midCol])
+        {
+            return true;
+        }
+        else if (target < matrix[midRow][midCol])
+        {
+            end = midCol - 1;
+        }
+        else
+        {
+            st = midCol + 1;
+        }
+    }
+    return false;
+}
+
+bool searchInMatrix(vector<vector<int>> matrix, int target)
+{
+    int row = matrix.size(), col = matrix[0].size();
+    int st = 0, end = row - 1;
+    while (st <= end)
+    {
+        int midRow = st + (end - st) / 2;
+        if (matrix[midRow][0] <= target && target <= matrix[midRow][col - 1])
+        {
+            // target is in midRow;
+            return searchInCol(matrix, target, midRow, col);
+        }
+        else if (target < matrix[midRow][0])
+        {
+            end = midRow - 1;
+        }
+        else
+        {
+            st = midRow + 1;
+        }
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int row = matrix.size(), col = matrix[0].size();
+    for (int i = col - 1; i >= 0; i--)
+    {
+        if (matrix[0][i] <= target)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                if (matrix[j][i] == target)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    int row = 4;
-    int matrix[row][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    int matrix1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int matrix2[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    vector<vector<int>> matrix3 = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+    vector<vector<int>> matrix4 = {{1}};
+    vector<vector<int>> matrix5 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+
     // pair<int, int> p = {-1, -1};
     // cout << p.first << " " << p.second;
 
-    int sum = maxColSum(matrix);
-    cout << sum;
+    // int sum = diagonalSum(matrix1, 3);
+    // cout << sum;
+
+    cout << searchMatrix(matrix3, 4);
 
     return 0;
 }
