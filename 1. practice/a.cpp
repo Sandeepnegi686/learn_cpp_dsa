9d46c196ba746490894322c620375ee5a74a6fb8
Sandeep <sandeepnegi686@gmail.com>
2025-09-14 11:01:19 +0530

    added iomanip association in settings and updated a.cpp for clarity

--- 1. practice/a.cpp
+++ 1. practice/a.cpp
@@ -15,13 +15,43 @@ int main()
     // cout << b << endl;
     // cout << c << endl;
 
-    int a = 100000;
-    int b = 100000;
-    cout << a * 1LL * b << endl;
+    // double a = 100000;
+    // double b = 100000;
+    // cout << fixed << setprecision(0) << a * b << endl;
 
-    int c = INT_MAX;
-    cout << c << endl;
-    cout << c + 2;
+    // int c = INT_MAX;
+    // cout << c << endl;
+    // cout << c + 2 << endl;
+
+    // double c = 1e5;
+    // cout << fixed << c;
+
+    // int a = 1;
+    // int mx = INT_MAX;
+    // cout << mx << endl;
+    // cout << mx + 1;
+    // int a = 2147483647;
+    // a = a + 1;
+    // a = a + 1;
+    // cout << a;
+    // int a = 1e9;
+    // int b = 1e9;
+    // cout << a * 1LL * b;
+
+    // for (int i = 1; i <= 10; i++){
+    //     for(int j = 1; j <= 2; j++){
+    //         if( i == 6){
+    //             break;
+    //         }
+    //         cout << " J = " << j << ", i = " << i << endl;
+    //     }
+    // }
+
+    int a = 11;
+    while (a--)
+    {
+        cout << a << endl;
+    }
 
     return 0;
 }
